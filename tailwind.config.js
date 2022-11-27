b9e2be9f96a4818e4ede87f294b29b3deff104e9
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:31:20 +0000

    feat: update tailwind config
    1. All containers now center in div my default.
    2. Add base animation style for loading recipe cards.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  experimental: {
-    optimizeUniversalDefaults: true
-  },
   theme: {
     extend: {
       container: {
+        center: true,
         padding: {
           DEFAULT: '0.5rem',
           sm: '1rem',
           lg: '2rem',
         },
       },
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',
+      }
     },
   },
 }
