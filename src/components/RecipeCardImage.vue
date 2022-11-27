470ce369ab05b160afba7a92792d31d1009484eb
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:07:15 +0000

    feat: add recipe card image

---
+++ src/components/RecipeCardImage.vue
@@ -0,0 +1,26 @@
+<script setup>
+  defineProps({
+    src: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <img
+    class="h-44 w-full rounded-xl object-cover object-center sm:h-auto sm:w-auto"
+    :src="src"
+    alt="image of recipe"
+    :width="width"
+    :height="height"
+  />
+</template>
