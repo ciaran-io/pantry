ef830366893e32a514b2420ab2de268db941d896
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:08:40 +0000

    feat: add recipe card favorites button

---
+++ src/components/RecipeFavoritesButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+  defineProps({
+    id: {
+      type: Number,
+      required: true,
+    },
+    userFavorites: {
+      type: Boolean,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <button class="btn relative w-full">
+    {{ userFavorites ? 'Remove from favorites' : 'Add to favorites' }}
+  </button>
+</template>
