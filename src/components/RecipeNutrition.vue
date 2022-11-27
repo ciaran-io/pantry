eecacf60763573bce154a9a97c09100bf7f95ad1
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:13:56 +0000

    feat: add recipe core nutrients component
    1. Displays macro nutrients on single recipe page

---
+++ src/components/RecipeNutrition.vue
@@ -0,0 +1,25 @@
+<script setup>
+  defineProps({
+    recipeCoreNutrients: {
+      type: Array ,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <p>Nutrition per serving</p>
+  <ul class="grid grid-flow-col">
+    <li
+      v-for="{ name, amount } in recipeCoreNutrients"
+      :key="name"
+      class="grid"
+    >
+      <span v-if="name === 'Carbohydrates'">{{ name.substring(0, 4) }}s</span>
+      <span v-else>{{ name }}</span>
+
+      <span v-if="name !== 'Calories'">{{ amount }}g</span>
+      <span v-else>{{ amount }}Kcal</span>
+    </li>
+  </ul>
+</template>
