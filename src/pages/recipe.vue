3bebf486649800d9730964fc3e3a80aaf876b3c9
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:25:38 +0000

    feat: complete recipe page

--- src/pages/recipe.vue
+++ src/pages/recipe.vue
@@ -2,128 +2,135 @@
   definePageMeta({
     layout: 'recipe',
   })
-  const route = useRoute()
-  const { id } = route.query
-  const config = useRuntimeConfig()
-  const apiKey = config.apiSpoonKey
 
-  const recipeEndpoint = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${apiKey}`
+  const route = useRoute()
+  const id = ref(route.query.id)
+  const activeMenu = ref(0)
+  const recipeMenuResultContainer = ref(null)
   const {
     data: recipe,
     pending,
     error,
-  } = await useLazyFetch(`${recipeEndpoint}`)
+  } = await useLazyFetch('/api/recipeById/', {
+    query: { recipeId: id.value },
+  })
 
-  const recipeCoreNutrients = ref()
-
+  const recipeCoreNutrients = ref([])
   const keyNutrients = ['Calories', 'Protein', 'Carbohydrates', 'Fat']
 
-  watch(
-    () => recipe.value,
-    () => {
-      const { nutrients } = recipe.value.nutrition
-      recipeCoreNutrients.value = nutrients.filter((nutrients) => {
-        let index = 0
-        for (index; index < keyNutrients.length; index++) {
-          if (nutrients.name === keyNutrients[index]) return nutrients.name
-        }
-      })
+  function getRecipeNutrients() {
+    const { nutrients } = recipe.value.nutrition
+    console.log(nutrients)
+
+    recipeCoreNutrients.value = nutrients.filter((nutrients) => {
+      let index = 0
+      for (index; index < keyNutrients.length; index++) {
+        if (nutrients.name === keyNutrients[index]) return nutrients.name
+      }
+    })
+  }
+
+  watch(recipe, (newRecipes, oldRecipes) => {
+    if (newRecipes) {
+      getRecipeNutrients()
     }
-  )
+  })
+
+  onMounted(() => {
+    if (recipeMenuResultContainer.value?.children[0]) {
+      const firstChildHeight =
+        recipeMenuResultContainer.value.children[0].clientHeight + 'px'
+      recipeMenuResultContainer.value.style.height = firstChildHeight
+    }
+  })
 </script>
 
 <template>
-  <div class="mt-16">
-    <div v-if="pending">Loading ...</div>
+  <div class="container mt-16">
+    <template v-if="error || pending">
+      <RecipeCardAnimation class="min-h-[235px]" />
+    </template>
     <div
       v-else
-      class="container mx-auto grid grid-cols-[1fr_1fr] gap-x-12"
+      class="grid gap-x-12 md:grid-cols-[1fr_1fr]"
     >
-      <div class="space-y-6 rounded-xl p-4 shadow">
+      <div class="container relative space-y-6 overflow-hidden">
         <!-- Recipe image -->
         <img
-          class="rounded-tl-3xl rounded-bl-3xl"
+          class="relative rounded-xl md:rounded-tl-3xl md:rounded-bl-3xl"
           :src="recipe.image"
           alt=""
         />
-        <!-- Recipe ingredients  -->
-        <div class="space-y-6">
-          <h3 class="text-3xl">Ingredients</h3>
-          <ul class="space-y-4">
-            <li
-              v-for="{ name, image, measures } in recipe.extendedIngredients"
-              :key="name"
-              class="flex items-center gap-x-6"
-            >
-              <img
-                class="h-12 w-12 rounded-full object-cover"
-                :src="`https://spoonacular.com/cdn/ingredients_100x100/${image}`"
-                alt=""
-                width="100"
-                height="100"
-              />
-              <div class="grow">{{ name }}</div>
-              <div>{{ Math.round(measures.metric.amount) }}g</div>
-            </li>
-          </ul>
+        <!-- Recipe menu mobile -->
+        <recipe-information-menu
+          :active-menu="activeMenu"
+          :title="recipe.title"
+          @change-menu="(index) => (activeMenu = index)"
+        />
+
+        <div
+          ref="recipeMenuResultContainer"
+          class="relative min-h-[750px] [&_>_div]:absolute [&_>_div]:top-0 [&_>_div]:left-0 [&_>_div]:w-full [&_>_div]:rounded-md [&_>_div]:p-1 [&_>_div]:shadow-md md:[&_>_div]:!relative"
+        >
+          <!-- Recipe ingredients  -->
+          <transition
+            v-show="activeMenu === 0"
+            name="slide-fade"
+          >
+            <recipe-ingredients
+              :extended-ingredients="recipe.extendedIngredients"
+              class="md:!p-4"
+            />
+          </transition>
+          <!-- Recipe instructions hidden on large screens-->
+          <transition
+            v-show="activeMenu === 1"
+            name="slide-fade"
+          >
+            <recipe-instructions :recipe="recipe" />
+          </transition>
+
+          <!-- Recipe nutrients hidden on large screens-->
+          <transition
+            v-show="activeMenu === 2"
+            name="slide-fade"
+          >
+            <div class="space-y-4">
+              <recipe-tags :recipe-tags="[recipe.vegetarian, recipe.vegan]" />
+              <recipe-nutrition :recipe-core-nutrients="recipeCoreNutrients" />
+            </div>
+          </transition>
         </div>
       </div>
       <!-- Recipe name & instructions -->
-      <div class="space-y-6 rounded-xl p-4 shadow">
-        <h1 class="text-4xl">{{ recipe.title }}</h1>
-        <div class="space-y-6">
-          <div>
-            <div class="space-x-2">
-              <span
-                v-if="!recipe.vegan"
-                class="rounded-full border px-4 py-1 text-sm"
-                >Vegan</span
-              >
-              <span
-                v-if="recipe.vegetarian"
-                class="rounded-full border px-4 py-1 text-sm"
-                >Vegetarian</span
-              >
-            </div>
-          </div>
-
-          <!-- Recipe nutrition -->
-          <p>Nutrition per serving</p>
-          <ul class="grid grid-flow-col">
-            <li
-              v-for="{ name, amount } in recipeCoreNutrients"
-              :key="name"
-              class="grid"
-            >
-              <span v-if="name === 'Carbohydrates'"
-                >{{ name.substring(0, 4) }}s</span
-              >
-              <span v-else>{{ name }}</span>
-
-              <span v-if="name !== 'Calories'">{{ amount }}g</span>
-              <span v-else>{{ amount }}Kcal</span>
-            </li>
-          </ul>
-
-          <h2 class="text-3xl">Instructions</h2>
-          <!-- Recipe instructions -->
-          <ol class="space-y-6">
-            <li
-              v-for="({ step }, index) in recipe.analyzedInstructions[0].steps"
-              class="grid grid-cols-[min-content_1fr] items-center gap-x-3"
-            >
-              <div
-                class="grid h-8 w-8 place-items-center rounded-full bg-slate-700"
-              >
-                <span class="block font-semibold text-white">
-                  {{ index + 1 }}
-                </span>
-              </div>
-              <span>{{ step }}</span>
-            </li>
-          </ol>
-        </div>
+      <div class="hidden space-y-8 rounded-xl p-4 shadow md:block">
+        <h1 class="text-4xl md:block">{{ recipe.title }}</h1>
+        <recipe-tags :recipe-tags="[recipe.vegetarian, recipe.vegan]" />
+        <recipe-nutrition :recipe-core-nutrients="recipeCoreNutrients" />
+        <recipe-instructions :recipe="recipe" />
       </div>
     </div>
   </div>
 </template>
+
+<style>
+  /*
+  src attribution vue js
+  https://vuejs.org/guide/built-ins/transition.html#css-based-transitions
+  Enter and leave animations can use different
+  durations and timing functions.
+*/
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-in 0.5s;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+</style>
