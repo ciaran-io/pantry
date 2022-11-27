39b051a2f9a80a5b2de3bcc6a0f6d7904af8eb18
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:12:24 +0000

    feat: add recipe instructions component
    1. Displays recipe cooking instructions in detail.

---
+++ src/components/RecipeInstructions.vue
@@ -0,0 +1,30 @@
+<script setup>
+  defineProps({
+    recipe: {
+      type: Object,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <div class="space-y-6">
+    <!-- Recipe instruction -->
+    <h2 class="text-3xl">Instructions</h2>
+    <!-- Recipe instructions -->
+    <ol class="space-y-6">
+      <li
+        v-for="({ step }, index) in recipe.analyzedInstructions[0].steps"
+        :key="index"
+        class="grid grid-cols-[min-content_1fr] items-center gap-x-3"
+      >
+        <div class="grid h-8 w-8 place-items-center rounded-full bg-slate-700">
+          <span class="block font-semibold text-white">
+            {{ index + 1 }}
+          </span>
+        </div>
+        <span>{{ step }}</span>
+      </li>
+    </ol>
+  </div>
+</template>
