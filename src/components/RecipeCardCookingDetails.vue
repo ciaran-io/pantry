7499aba02051bc3119b339c3a3b7bb9e3c272e17
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:06:56 +0000

    feat: add cooking details components
    1. Displays cooking times & servings per recipe.

---
+++ src/components/RecipeCardCookingDetails.vue
@@ -0,0 +1,34 @@
+<script setup>
+// Template for cooking time & servings
+  defineProps({
+    cookingTime: {
+      type: String,
+      required: true,
+    },
+    servings: {
+      type: Number,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <div class="mt-auto flex justify-between text-sm">
+    <!-- Cooking time -->
+    <div>
+      <span class="block text-gray-500">Cook Time</span>
+      <span class="block font-bold">
+        {{ cookingTime }}
+      </span>
+    </div>
+      <!-- Cooking servings -->
+    <div>
+      <span class="block text-gray-500">Serving</span>
+      <span class="block font-bold">
+        <Icon name="fluent:people-48-filled" />
+        {{ servings }}
+      </span>
+    </div>
+
+  </div>
+</template>
