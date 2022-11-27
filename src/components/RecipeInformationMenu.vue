e8e4ddc0690b1e888d28d8f17113137212fc1812
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:09:32 +0000

    feat: add recipe information menu
    1. Used on mobile devices to toggle recipe menus.

---
+++ src/components/RecipeInformationMenu.vue
@@ -0,0 +1,38 @@
+<script setup>
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    activeMenu: {
+      type: Number,
+      required: true,
+    },
+  })
+  defineEmits(['changeMenu'])
+  const recipeNavMenu = new Set([
+    'Ingredients',
+    'Instructions',
+    'Nutritional Information',
+  ])
+</script>
+
+<template>
+  <div class="space-y-4 md:hidden">
+    <h1 class="text-4xl">{{ title }}</h1>
+    <ul class="flex justify-between gap-x-4 overflow-y-auto pb-4">
+      <li
+        v-for="(list, index) in recipeNavMenu"
+        :key="list"
+        class="min-w-max rounded-full border-2 px-3 py-0.5 transition-colors duration-200 cursor-pointer  "
+        :class="[
+          { 'border-teal-500 ': activeMenu === index },
+          { 'border-transparent': activeMenu !== index },
+        ]"
+        @click="$emit('changeMenu', index)"
+      >
+        {{ list }}
+      </li>
+    </ul>
+  </div>
+</template>
