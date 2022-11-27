3bac49ece6c6ed0f07606fb83aeacb2da85e6523
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:19:02 +0000

    feat: add header navigation component
    1. Complete navigation for mobile & desktop

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,28 +1,50 @@
 <script setup>
+  const route = useRoute()
   const navLinks = [
     {
       label: 'Home',
-      url: '/',
+      url: '',
     },
     {
       label: 'About',
       url: 'about',
     },
     {
-      label: 'Favourites',
-      url: 'favourites',
+      label: 'Favorites',
+      url: 'favorites',
     },
   ]
+
+  const favorites = userFavorites()
+  const isMobileMenuClosed = ref(true)
+  const mobileSize = 640
+
+  // Close mobile menu onn screen sizes
+  // greater than mobileSize
+  onMounted(() => {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > mobileSize) {
+        isMobileMenuClosed.value = false
+      }
+    })
+  })
+  watch(
+    () => route.path,
+    () => (isMobileMenuClosed.value = false)
+  )
+  onMounted(() => (isMobileMenuClosed.value = false))
 </script>
 
 <template>
   <div class="md:container md:mx-auto md:grid md:grid-flow-col">
-    <header class="container mx-auto mt-2 space-y-12 md:mt-12">
-      <nav class="flex items-center justify-between">
+    <header class="container mx-auto mt-6 space-y-12 md:mt-12">
+      <nav class="relative flex items-center justify-between">
         <nuxt-link to="/">Pantry</nuxt-link>
-        
         <!-- Mobile menu button -->
-        <button class="sm:hidden">
+        <button
+          class="sm:hidden"
+          @click="isMobileMenuClosed = !isMobileMenuClosed"
+        >
           <Icon
             class="h-8 w-8"
             name="ic:baseline-menu"
@@ -30,14 +52,25 @@
         </button>
 
         <!-- Main navigation -->
-        <ul class="hidden sm:flex sm:gap-x-6">
+        <ul
+          :class="{ hidden: !isMobileMenuClosed }"
+          class="absolute top-8 left-0 z-50 w-full space-y-4 bg-slate-200 p-4 sm:relative sm:top-0 sm:flex sm:w-auto sm:gap-x-6 sm:space-y-0 sm:bg-inherit sm:p-0"
+        >
           <li
-            v-for="links in navLinks"
-            :key="links.label"
-            :to="`/${links.url}`"
+            v-for="link in navLinks"
+            :key="link.label"
           >
-            <nuxt-link>
-              {{ links.label }}
+            <nuxt-link
+              class="relative block w-full"
+              :to="`/${link.url}`"
+            >
+              {{ link.label }}
+              <span
+                v-if="link.label === 'Favorites'"
+                class="rounded-full bg-[#939e52] py-1 px-2 text-xs text-white md:absolute md:-right-3 md:-top-5"
+              >
+                {{ favorites.size }}
+              </span>
             </nuxt-link>
           </li>
         </ul>
@@ -47,14 +80,13 @@
         <section
           class="space-y-12 md:grid md:min-h-[500px] md:grid-cols-[2fr,1fr] md:space-y-0"
         >
-
           <!-- Banner text -->
           <div class="pr-8">
             <h1
               class="text-3xl font-bold !leading-[1.2] md:max-w-[16ch] md:text-5xl"
             >
               Welcome to your <span class="text-[#939e52]">Pantry</span>, find
-              all your favourite <span class="text-[#939e52]">meals</span> &
+              all your favorite <span class="text-[#939e52]">meals</span> &
               <span class="text-[#939e52]">recipes</span>
             </h1>
           </div>
@@ -66,10 +98,10 @@
             <div
               class="plate absolute -left-12 w-11/12 md:-left-20 md:top-1/4 md:bottom-10 md:h-3/4 md:w-full"
             >
-              <img
-                src="~assets/images/subject.png"
-                alt=""
-                class="relative md:w-full rounded-full border-8 border-white p-2"
+              <nuxt-img
+                src="/assets/images/subject.png"
+                alt="image of a plate with salad"
+                class="relative rounded-full border-8 border-white p-2 md:w-full"
                 height="225"
                 width="200"
               />
@@ -80,10 +112,12 @@
     </header>
     <!-- Aside image -->
     <div class="hidden lg:mt-14 lg:block lg:w-80">
-      <img
+      <nuxt-img
+        quality="90"
+        format="webp"
         class="h-[565px] w-auto rounded-tr-3xl rounded-br-3xl object-cover"
-        src="~assets/images/ingredients.jpg"
-        alt=""
+        src="/assets/images/ingredients.jpg"
+        alt="image of ingredients, avocado, salt and toast"
         height="1000"
         width="405"
       />
--- src/layouts/recipe.vue
+++ src/layouts/recipe.vue
@@ -1,29 +1,51 @@
 <script setup>
+  const favorites = userFavorites()
+  const route = useRoute()
   const navLinks = [
     {
       label: 'Home',
-      url: '/',
+      url: '',
     },
     {
       label: 'About',
       url: 'about',
     },
     {
-      label: 'Favourites',
-      url: 'favourites',
+      label: 'Favorites',
+      url: 'favorites',
     },
   ]
-</script>
+  const isMobileMenuClosed = ref(true)
+  const mobileSize = 640
 
+  // Close mobile menu onn screen sizes
+  // greater than mobileSize
+  onMounted(() => {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > mobileSize) {
+        isMobileMenuClosed.value = false
+      }
+    })
+  })
+
+  watch(
+    () => route.path,
+    () => (isMobileMenuClosed.value = false)
+  )
+  onMounted(() => (isMobileMenuClosed.value = false))
+</script>
 
 <template>
   <div>
-    <header class="container mx-auto mt-2 space-y-12 md:mt-12">
-      <nav class="flex items-center justify-between">
+    <header class="container mx-auto mt-6 space-y-12 md:mt-12">
+      <nav class="relative flex items-center justify-between">
         <nuxt-link to="/">Pantry</nuxt-link>
-        
+
         <!-- Mobile menu button -->
-        <button class="sm:hidden">
+        <button
+          class="sm:hidden"
+          @click="isMobileMenuClosed = !isMobileMenuClosed"
+        >
           <Icon
             class="h-8 w-8"
             name="ic:baseline-menu"
@@ -31,20 +53,31 @@
         </button>
 
         <!-- Main navigation -->
-        <ul class="hidden sm:flex sm:gap-x-6">
+        <ul
+          :class="{ hidden: !isMobileMenuClosed }"
+          class="absolute top-8 left-0 z-50 w-full space-y-4 bg-slate-200 p-4 sm:relative sm:top-0 sm:flex sm:w-auto sm:gap-x-6 sm:space-y-0 sm:bg-inherit sm:p-0"
+        >
           <li
-            v-for="links in navLinks"
-            :key="links.label"
-            :to="`/${links.url}`"
+            v-for="link in navLinks"
+            :key="link.label"
           >
-            <nuxt-link>
-              {{ links.label }}
+            <nuxt-link
+              class="relative block w-full"
+              :to="`/${link.url}`"
+            >
+              {{ link.label }}
+              <span
+                v-if="link.label === 'Favorites'"
+                class="rounded-full bg-[#939e52] py-1 px-2 text-xs text-white md:absolute md:-right-3 md:-top-5"
+              >
+                {{ favorites.size }}
+              </span>
             </nuxt-link>
           </li>
         </ul>
       </nav>
     </header>
     <slot />
-    <LazyTheFooter/>
+    <LazyTheFooter />
   </div>
 </template>
