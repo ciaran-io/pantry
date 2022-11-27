822c6e397abd6fc5b339bf5eaa5658186b21741f
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:02:03 +0000

    feat: add recipe card animation
    1. Used as placeholder when the recipe request is pending or error.

---
+++ src/components/RecipeCardAnimation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="flex h-max bg-white animate-pulse space-x-4 rounded-xl border border-gray-300 p-4 shadow "
+  >
+    <div class="h-10 w-10 rounded-full bg-slate-700"></div>
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 rounded bg-slate-700"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="col-span-2 h-2 rounded bg-slate-700"></div>
+          <div class="col-span-1 h-2 rounded bg-slate-700"></div>
+        </div>
+        <div class="h-2 rounded bg-slate-700"></div>
+      </div>
+    </div>
+  </div>
+</template>
