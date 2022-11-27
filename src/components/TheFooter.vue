9c956bf41e1a8b94e075600e7a54891556449c61
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-11-27 04:17:50 +0000

    feat: add footer information
    1. Add social links and copyright text

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,29 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const socialLinks = {
+    facebook: 'logos:facebook',
+    twitter: 'logos:twitter',
+    instagram: 'bxl:instagram-alt',
+    pinterest: 'logos:pinterest',
+  }
+  const currentYear = computed(() => new Date().getFullYear())
+</script>
 
 <template>
-  <div>
-    Component: TheFooter
-  </div>
+  <footer class="container mt-32 border-t">
+    <div class="grid h-64 place-items-center">
+      <div class="flex gap-x-4">
+        <nuxt-link
+          v-for="(value, key) in socialLinks"
+          :key="key"
+          :to="`https://${key}.com`"
+        >
+          <Icon
+            class="h-8 w-8"
+            :name="value"
+          />
+        </nuxt-link>
+      </div>
+      <div>©{{ currentYear }} Pantry</div>
+    </div>
+  </footer>
 </template>
-
-<style scoped></style>
